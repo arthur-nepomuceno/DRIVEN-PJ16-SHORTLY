322a6c8bd940bcddeac537706f5d235fd4410f87
Arthur Nepomuceno <arth.nepomuceno@gmail.com>
2022-08-06 17:50:02 -0300

    [feature] DELETE /urls/:id

--- src/routers/urlRouter.js
+++ src/routers/urlRouter.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import postUrl from "../controllers/urlcontrollers/postUrl.js";
 import getUrlById from "../controllers/urlcontrollers/getUrlById.js";
 import getShortUrl from "../controllers/urlcontrollers/getShortUrl.js";
+import deleteUrlById from "../controllers/urlcontrollers/deleteUrlById.js";
 import urlShortenMiddleware from "../middlewares/urlShortenMiddleware.js";
 
 const urlRouter = Router();
@@ -9,5 +10,6 @@ const urlRouter = Router();
 urlRouter.post('/urls/shorten', urlShortenMiddleware ,postUrl);
 urlRouter.get('/urls/:id', getUrlById);
 urlRouter.get('/urls/open/:shortUrl', getShortUrl)
+urlRouter.delete('/urls/:id', deleteUrlById);
 
 export default urlRouter;
