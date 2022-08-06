96d9ab0bb9bd92961408b3152f0bc548a8f7c292
Arthur Nepomuceno <arth.nepomuceno@gmail.com>
2022-08-06 17:08:02 -0300

    [feature] route GET /urls/open/:shortUrl

--- src/routers/urlRouter.js
+++ src/routers/urlRouter.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import postUrl from "../controllers/urlcontrollers/postUrl.js";
 import getUrlById from "../controllers/urlcontrollers/getUrlById.js";
+import getShortUrl from "../controllers/urlcontrollers/getShortUrl.js";
 import urlShortenMiddleware from "../middlewares/urlShortenMiddleware.js";
 
 const urlRouter = Router();
 
 urlRouter.post('/urls/shorten', urlShortenMiddleware ,postUrl);
 urlRouter.get('/urls/:id', getUrlById);
+urlRouter.get('/urls/open/:shortUrl', getShortUrl)
 
 export default urlRouter;
