86fd653fb8b1a12fb4d28a5d5678624daef46447
Arthur Nepomuceno <arth.nepomuceno@gmail.com>
2022-08-07 15:07:50 -0300

    [feature] route GET/ranking

--- src/controllers/userControllers/getRanking.js
+++ src/controllers/userControllers/getRanking.js
@@ -1,3 +1,21 @@
+import connection from '../../dbStrategy/postgres.js';
+
 export default async function getRanking(req, res){
-    res.send('get ranking ok');
+    const getQuery = `SELECT "userId" AS id, 
+                              users.name, 
+                              COUNT("userId") AS "linksCount", 
+                              SUM("visitCount") AS "visitCount" 
+                      FROM "shortUrls" 
+                      JOIN users 
+                      ON users.id = "shortUrls"."userId" 
+                      GROUP BY "userId", users.name 
+                      ORDER BY "visitCount" DESC 
+                      LIMIT 10;`
+                               
+    try{
+        const {rows: queryResult} = await connection.query(getQuery);
+        res.send(queryResult);
+    } catch(error) {
+        res.status(500).send(error);
+    }
 }
